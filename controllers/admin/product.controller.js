f0b022f21c555e58dee55217bcb8683917d72ab0
GiapNguyenn <truongnguyengiaps@gmail.com>
2026-05-20 20:43:23 +0700

    update xoa mem

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -85,7 +85,12 @@ module.exports.changeMulti = async (req ,res) => {
 module.exports.deleteItem = async (req, res) => {
   const id = req.params.id;
  // await Product.deleteOne({ _id : id});
-  await Product.updateOne({ _id : id }, {deleted : true})
+  await Product.updateOne({ _id : id }, 
+    {
+      deleted : true ,
+      deletedAt : new Date()
+    }
+  )
    res.redirect("back");
 }
   
