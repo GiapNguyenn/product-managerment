b4c1227e1e6b7119d8be068d0c41491d8e5abc38
GiapNguyenn <truongnguyengiaps@gmail.com>
2026-05-30 20:45:13 +0700

    upload  image tu local  cho thông tin khi tao san pham

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -125,6 +125,7 @@ module.exports.create = async (req, res) => {
 }
 // [POST] /admin/product/create
 module.exports.createPost = async (req, res) => {
+  console.log(req.file);
   req.body.price = parseInt(req.body.price);
   req.body.discountPercentage = parseInt(req.body.discountPercentage);
   req.body.stock = parseInt(req.body.stock);
@@ -134,6 +135,7 @@ module.exports.createPost = async (req, res) => {
   }else{
     req.body.position = parseInt(req.body.position);
   }
+  req.body.thumbnail = `/uploads/${req.file.filename}`;
   const product = new Product (req.body);
     await product.save();
   res.redirect(`${systemConfig.perfixAdmin}/products`)
--- routers/admin/product.route.js
+++ routers/admin/product.route.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router= express.Router();
+const multer = require("multer")
 const controller = require("../../controllers/admin/product.controller")
+const storageMulter = require("../../helpers/storageMulter")
+const upload = multer({ storage: storageMulter() })
+
 router.get('/',controller.products);
 router.patch('/change-status/:status/:id', controller.changeStatus );
 router.patch('/change-multi', controller.changeMulti);
 router.delete('/delete/:id', controller.deleteItem);
 router.get('/create', controller.create);
-router.post('/create', controller.createPost);
-module.exports = router
+router.post('/create',upload.single("thumbnail") ,controller.createPost);
+module.exports = router  
 // 1h03p
